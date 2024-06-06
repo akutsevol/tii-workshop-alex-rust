75d455fd8ca38cec2c0cf0e2aabfea6957c70659
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-06 16:25:43 +0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+
+//use std::env;
+
+pub fn foo(mut a: i32, lim: i32) {
+    while a < lim {
+        a += 1;
+        if a % 2 != 0 {
+            continue;
+        }
+        println!("0x{a:04X?}");   // ? - for debugging
+    }
+}
+
+fn main() {
+    //env::set_var("RUST_BACKTRACE", "1"); // use for backtrace!
+
+    println!("Hello, world!");
+    foo(1, 10);
+    print_num("8");
+    print_num("n");
+}
+
+pub fn bar(a: u64) -> u64 { a as u64 }
+
+fn print_num(string_number: &str) {
+    match string_number.parse::<i32>() {
+        Ok(number)  => println!("Ваше число – {}", number),
+        Err(e) => println!("Ошибка: {}", e)
+    }
+    bar(5);
+    //string_number.parse::<i32>().unwrap();
+}
