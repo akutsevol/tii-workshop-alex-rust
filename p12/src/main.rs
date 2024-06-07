f0870290b0849d36eecbe110a4cdc2884701263a
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-07 09:34:43 +0400

    Error handling 2-1.pdf[16]

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -6,9 +6,13 @@ fn main () {
     #[warn(unused_variables)]
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err (error) => match error.kind() {
+        Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
-                
+                Ok(fs) => fs,
+                Err(e) => panic!("Problem creating file {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening file: {:?}", other_error);
             }
         }
     };
