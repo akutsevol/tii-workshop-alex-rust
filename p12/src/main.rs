a6b30e14c546cab1fea594633be69b0c3ba2aea6
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-07 10:01:33 +0400

    basic threads

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -1,11 +1,23 @@
 use std::time::Duration;
 
 fn main () {
-    let h = std::thread::spawn(|| {
-        println!("Hi from thread!");
-        std::thread::sleep(Duration::from_secs(3));
-        42u32
+    let buf = vec!(1u32, 2, 3);
+
+    let res = std::thread::scope(|s| {
+        let h1 = s.spawn(|| {
+            let a: u32  = buf.iter().sum();
+            println!("Hi from thread {}!", a);
+            std::thread::sleep(Duration::from_secs(1));
+            2u32
+        });
+        let h2 = s.spawn(|| {
+            let b: u32 = buf.iter().map(|&x| x * x).sum();
+            println!("Hi from thread {}!", b);
+            std::thread::sleep(Duration::from_secs(3));
+            42u32
+        });
+        h1.join().unwrap() + h2.join().unwrap()
     });
-    let res = h.join().unwrap();
-    println!("{:?}", res);
+
+    println!("{res}");
 }
