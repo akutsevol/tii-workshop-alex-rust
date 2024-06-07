1934958d11fcfcef42ef34958f1b597ec0751d46
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-07 09:24:02 +0400

    Error handling 2-1.pdf[15]

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -1,32 +1,14 @@
+use std::fs::File;
 
-//use std::env;
-
-pub fn foo(mut a: i32, lim: i32) {
-    while a < lim {
-        a += 1;
-        if a % 2 != 0 {
-            continue;
-        }
-        println!("0x{a:04X?}");   // ? - for debugging
-    }
+fn main () {
+    let greeting_file_result = File::open("hello.txt");
+    #[warn(unused_variables)]
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err (error) => panic!("Problem opening the file: {:?})", error),
+    };
 }
 
-fn main() {
-    //env::set_var("RUST_BACKTRACE", "1"); // use for backtrace!
-
-    println!("Hello, world!");
-    foo(1, 10);
-    print_num("8");
-    print_num("n");
-}
-
-pub fn bar(a: u64) -> u64 { a as u64 }
-
-fn print_num(string_number: &str) {
-    match string_number.parse::<i32>() {
-        Ok(number)  => println!("Ваше число – {}", number),
-        Err(e) => println!("Ошибка: {}", e)
-    }
-    bar(5);
-    //string_number.parse::<i32>().unwrap();
-}
+// fn main() {
+//     let greeting_file_result = File::open("hello.txt").unwrap();
+// }
