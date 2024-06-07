c0d062b30004aedf5ba7b5a328ce8962a40b4f6f
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-07 10:28:03 +0400

    basic threads

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -6,14 +6,14 @@ fn main () {
     let res = std::thread::scope(|s| {
         let h1 = s.spawn(|| {
             let a: u32  = buf.iter().sum();
-            println!("Hi from thread {}!", a);
             std::thread::sleep(Duration::from_secs(1));
+            println!("Hi from thread {}!", a);
             2u32
         });
         let h2 = s.spawn(|| {
             let b: u32 = buf.iter().map(|&x| x * x).sum();
-            println!("Hi from thread {}!", b);
             std::thread::sleep(Duration::from_secs(3));
+            println!("Hi from thread {}!", b);
             42u32
         });
         h1.join().unwrap() + h2.join().unwrap()
