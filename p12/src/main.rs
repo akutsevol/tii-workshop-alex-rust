8b138d52f235a8aa08c66cccdc3a0526d7066474
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-07 09:27:39 +0400

    Error handling 2-1.pdf[16]

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -1,14 +1,15 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main () {
     let greeting_file_result = File::open("hello.txt");
     #[warn(unused_variables)]
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err (error) => panic!("Problem opening the file: {:?})", error),
+        Err (error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                
+            }
+        }
     };
 }
-
-// fn main() {
-//     let greeting_file_result = File::open("hello.txt").unwrap();
-// }
